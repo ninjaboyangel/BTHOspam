c33fc7989cf7dadbeb680d99c423d19237725512
Noah Lee <ninjaboyangel@gmail.com>
2019-09-08 01:59:27 -0500

    fixed filepath to non hard-code

--- spamfilterer.py
+++ spamfilterer.py
@@ -1,17 +1,21 @@
 # This Python file uses the following encoding: utf-8
 from PyQt5 import QtWidgets, uic
 import sys
+import os
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('C:/Users/Noah/Documents/BTHOspam/test.ui', self)
+        cwd = os.getcwd()
+        print(cwd)
+        uic.loadUi(cwd + '/Documents/BTHOspam/test.ui', self)
 
 
         self.button = self.findChild(QtWidgets.QPushButton, 'testButton')
         self.button.clicked.connect(self.testButtonPressed)
 
         self.show()
+
 
     def testButtonPressed(self):
         print('testButtonPressed')
